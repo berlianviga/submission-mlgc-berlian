a7eac86633d33c4d2441f46004f681d2568f5c51
Berlian Viga <berlianviga@gmail.com>
2024-12-04 00:41:35 +0700

    modified server.js

--- src/server/server.js
+++ src/server/server.js
@@ -6,8 +6,8 @@ const InputError = require("../exceptions/InputError");
 
 (async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || "localhost",
     routes: {
       cors: {
         origin: ["*"],
